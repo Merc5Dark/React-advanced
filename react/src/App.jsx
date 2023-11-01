6d2bfbf230cef53711bd691da22414bb172e17dd
Merc5Dark <31131794siya@gmail.com>
2023-11-01 09:53:06 +0200

    commit

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <h1>Hello META</h1>
+    </>
+  )
+}
+
+export default App
---
+++ react/src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './NavBar.css'; 
+
+const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="logo">Logo</div>
+            <div className={`menu-toggle ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+            <ul className={`menu ${menuOpen ? 'open' : ''}`}>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
